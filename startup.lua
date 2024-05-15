2cf25e9e00011030f27fbe5a8db5f5fed129287f
Joshua Kool <177300@student.horizoncollege.nl>
2024-05-15 16:26:42 +0200

    test

--- startup.lua
+++ startup.lua
@@ -5,11 +5,16 @@ if monitor == nil then
     print("No monitor found")
     return
 else
+    local buttoncollor = colors.red
+
     local function getClick()
         local event, side, x, y = os.pullEvent("monitor_touch")
         shell.run("loading.lua")
     end
     while true do
         getClick()
+        if getClick() then
+            break
+        end
     end
 end
