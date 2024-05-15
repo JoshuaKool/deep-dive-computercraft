a4509284357ade3d60b7d908eaafa945825cafef
Joshua Kool <177300@student.horizoncollege.nl>
2024-05-15 12:28:10 +0200

    loading

--- loading.lua
+++ loading.lua
@@ -1,3 +1,57 @@
-print("Welcome to the turtle program!")
-sleep(3)
-print("test")
+local monitor = peripheral.find("monitor")
+
+if monitor == nil then
+    print("No monitor found")
+    return
+else
+    local y = 1
+    monitor.write("Welcome to the turtle program!")
+    monitor.setTextScale(0.5)
+    monitor.write("1")
+    sleep(0.25)
+    monitor.setCursorPos(1, y)
+    y = y + 1
+    monitor.write("1")
+    sleep(0.25)
+    monitor.setCursorPos(1, y)
+    y = y + 1
+    monitor.write("1")
+    sleep(0.25)
+    monitor.setCursorPos(1, y)
+    y = y + 1
+    monitor.write("1")
+    sleep(0.25)
+    monitor.setCursorPos(1, y)
+    y = y + 1
+    monitor.write("1")
+    sleep(0.25)
+    monitor.setCursorPos(1, y)
+    y = y + 1
+    monitor.write("1")
+    sleep(0.25)
+    monitor.setCursorPos(1, y)
+    y = y + 1
+    monitor.write("1")
+    sleep(0.25)
+    monitor.setCursorPos(1, y)
+    y = y + 1
+    monitor.write("1")
+    sleep(0.25)
+    monitor.setCursorPos(1, y)
+    y = y + 1
+    monitor.write("1")
+    sleep(0.25)
+    monitor.setCursorPos(1, y)
+    y = y + 1
+    monitor.write("1")
+    sleep(0.25)
+    monitor.setCursorPos(1, y)
+    y = y + 1
+    monitor.write("Loading control buttons")
+    sleep(0.25)
+    monitor.setCursorPos(1, y)
+    y = y + 1
+    monitor.write("Succes")
+    sleep(0.25)
+    monitor.clear()
+end
