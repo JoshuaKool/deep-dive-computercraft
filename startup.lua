799dc021a16027260f192ce379308def0e83f55e
Joshua Kool <177300@student.horizoncollege.nl>
2024-05-15 15:17:53 +0200

    test

--- startup.lua
+++ startup.lua
@@ -4,11 +4,14 @@ if monitor == nil then
     print("No monitor found")
     return
 else
+    
     local aButton = main:addButton():setText("Click")
 
-    aButton:onClick(function(self,event,button,x,y)
-    if(event=="mouse_click")and(button==1)then
-        monitor.debug("Left mousebutton got clicked!")
-    end
-    end)
+  aButton:onClick(function(self,event,button,x,y)
+  if(event=="mouse_click")and(button==1)then
+    monitor.write("Button clicked")
+    sleep(2)
+    monitor.clear()
+  end
+end)
 end
