75e72f90b697b52d1e7f72879212e0aba21c01c4
Joshua Kool <177300@student.horizoncollege.nl>
2024-05-15 13:49:00 +0200

    loading

--- loading.lua
+++ loading.lua
@@ -5,53 +5,60 @@ if monitor == nil then
     return
 else
     local y = 1
-    monitor.write("Welcome to the turtle program!")
+    local x = 2
     monitor.setTextScale(0.5)
+    monitor.setCursorPos(x, y)
+    monitor.write("Welcome to the turtle program!")
+    sleep(0.25)
+    y = y + 1
+    monitor.setCursorPos(x, y)
     monitor.write("1")
     sleep(0.25)
-    monitor.setCursorPos(1, y)
     y = y + 1
+    monitor.setCursorPos(x, y)
     monitor.write("1")
     sleep(0.25)
-    monitor.setCursorPos(1, y)
     y = y + 1
+    monitor.setCursorPos(x, y)
     monitor.write("1")
     sleep(0.25)
-    monitor.setCursorPos(1, y)
     y = y + 1
+    monitor.setCursorPos(x, y)
     monitor.write("1")
     sleep(0.25)
-    monitor.setCursorPos(1, y)
     y = y + 1
+    monitor.setCursorPos(x, y)
     monitor.write("1")
     sleep(0.25)
-    monitor.setCursorPos(1, y)
     y = y + 1
+    monitor.setCursorPos(x, y)
     monitor.write("1")
     sleep(0.25)
-    monitor.setCursorPos(1, y)
     y = y + 1
+    monitor.setCursorPos(x, y)
     monitor.write("1")
     sleep(0.25)
-    monitor.setCursorPos(1, y)
     y = y + 1
+    monitor.setCursorPos(x, y)
     monitor.write("1")
     sleep(0.25)
-    monitor.setCursorPos(1, y)
     y = y + 1
+    monitor.setCursorPos(x, y)
     monitor.write("1")
     sleep(0.25)
-    monitor.setCursorPos(1, y)
     y = y + 1
+    monitor.setCursorPos(x, y)
     monitor.write("1")
     sleep(0.25)
-    monitor.setCursorPos(1, y)
     y = y + 1
+    monitor.setCursorPos(x, y)
     monitor.write("Loading control buttons")
     sleep(0.25)
-    monitor.setCursorPos(1, y)
     y = y + 1
+    monitor.setCursorPos(x, y)
     monitor.write("Succes")
-    sleep(0.25)
+    sleep(1)
     monitor.clear()
 end
+
+shell.run("button.lua")
