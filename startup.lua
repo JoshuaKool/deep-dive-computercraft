3494d78e43bcbe677f5d1307324567a94460f8d9
Joshua Kool <177300@student.horizoncollege.nl>
2024-05-15 15:00:07 +0200

    startup test code

--- startup.lua
+++ startup.lua
@@ -7,6 +7,7 @@ end
 
 local main = basalt.createFrame()
 local aButton = main:addButton():setText("Click")
+local x, y = monitor.getSize()
 
 aButton:onClick(function(self,event,button,x,y)
   if(event=="mouse_click")and(button==1)then
