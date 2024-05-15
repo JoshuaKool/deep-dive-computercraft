2e1982c11da546aa5a2fec5132c8ee1a8a205e3f
Joshua Kool <177300@student.horizoncollege.nl>
2024-05-15 15:07:42 +0200

    test

--- startup.lua
+++ startup.lua
@@ -4,12 +4,11 @@ if monitor == nil then
     print("No monitor found")
     return
 else
-    local main = basalt.createFrame()
     local aButton = main:addButton():setText("Click")
 
     aButton:onClick(function(self,event,button,x,y)
     if(event=="mouse_click")and(button==1)then
-        basalt.debug("Left mousebutton got clicked!")
+        monitor.debug("Left mousebutton got clicked!")
     end
     end)
 end
