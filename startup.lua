d3cf7154830909131197b4b5562b26a666ba0214
Joshua Kool <177300@student.horizoncollege.nl>
2024-05-16 14:44:45 +0200

    startup

--- startup.lua
+++ startup.lua
@@ -2,23 +2,30 @@ local mouseWidth = 0
 local mouseHeight = 0
 local monitor = peripheral.find("monitor")
 
-
 if monitor == nil then
     print("No monitor found")
     return
 else
-    local x, y = monitor.getSize()
+    local monitorWidth, monitorHeight = monitor.getSize()
     monitor.clear()
     monitor.setBackgroundColor(colors.green)
     monitor.setTextColor(colors.white)
-    monitor.setCursorPos(1, 2)
-    monitor.write("Start")
+
+    local startText = "Start"
+    local startTextWidth = #startText
+    local startX = math.floor((monitorWidth - startTextWidth) / 2) + 1
+    local startY = math.floor(monitorHeight / 2)
+
+    monitor.setCursorPos(startX, startY)
+    monitor.write(startText)
 
     local function checkClickPosition()
-        if mouseWidth >= 0 and mouseWidth <= 6 and mouseHeight > 1 and mouseHeight < 3 then
-            shell.run("loading.lua")
+        if mouseWidth >= startX and mouseWidth <= startX + startTextWidth - 1
+            and mouseHeight == startY then
+            os.run({}, "loading.lua")
         end
     end
+
     while true do
         local event, side, x, y = os.pullEvent()
         if event == "monitor_touch" then
@@ -26,7 +33,6 @@ else
             mouseHeight = y
             monitor.setBackgroundColor(colors.black)
             checkClickPosition()
-            return false
         end
     end
 end
