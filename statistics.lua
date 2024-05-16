ce39f6692f8af68e391dfd07977a6d0a425da46e
Joshua Kool <177300@student.horizoncollege.nl>
2024-05-16 19:44:05 +0200

    statistic verticalgit

--- statistics.lua
+++ statistics.lua
@@ -10,25 +10,26 @@ monitor.clear()
 
 local function drawVerticalProgressBar(monitor, usedItems, totalSlots)
     local width, height = monitor.getSize()
-    local barHeight = height
+    local barHeight = height - 1
     local filledHeight = math.floor((usedItems / (totalSlots * 64)) * barHeight)
     local emptyHeight = barHeight - filledHeight
     local barXPos = math.floor(width / 2) 
 
     for y = 1, filledHeight do
-        monitor.setCursorPos(barXPos, height - y - 1)
+        monitor.setCursorPos(barXPos, height - y)
         monitor.setBackgroundColor(colors.green)
         monitor.write(" ")
     end
 
     for y = filledHeight + 1, barHeight do
-        monitor.setCursorPos(barXPos, height - y - 1)
+        monitor.setCursorPos(barXPos, height - y)
         monitor.setBackgroundColor(colors.red)
         monitor.write(" ")
     end
 
     monitor.setBackgroundColor(colors.black)
 end
+
 
 
 local function countItems(inventory)
