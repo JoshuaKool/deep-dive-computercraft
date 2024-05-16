df482afb7be45cb1732d59987fc56777844ffc92
Joshua Kool <177300@student.horizoncollege.nl>
2024-05-16 12:11:25 +0200

    test

--- statistics.lua
+++ statistics.lua
@@ -1,8 +1,6 @@
 local monitor = peripheral.find("monitor")
 local chest = peripheral.find("minecraft:chest")
-local shulker = peripheral.find("minecraft:shulker_box")
 local barrel = peripheral.find("minecraft:barrel")
-
 
 monitor.clear()
 
@@ -11,42 +9,53 @@ if monitor == nil then
     return
 end
 
-if chest == "minecraft:chest" then
-    iets = chest
-    else if shulker == "minecraft:shulker_box" then
-        iets = shulker
+function print_inventory_statistics(chest, barrel, monitor)
+    local x, y = monitor.getSize()
+    monitor.setCursorPos(1, 1)
+    monitor.write(x)
+    monitor.setCursorPos(1, 2)
+    monitor.write(y)
+
+    if x < 36 then
+        monitor.setTextScale(4)
+        monitor.write("Monitor width is too small")
+    else if y < 13 then
+        monitor.setTextScale(6)
+        monitor.write("Monitor high is too small")
+    end
+    end
+    if chest == "minecraft:chest" then
+        local inventory = chest.list()
+
+        for chest in pairs(inventory) do
+            chest.name = chest.name:gsub("minecraft:", "")
+            local text2 = chest.name
+            local text = "chest"
+            local width = math.floor((x - #text) / 2)
+            local height = 1
+            monitor.setCursorPos(width, height)
+            monitor.write(text)
+        end
         else if barrel == "minecraft:barrel" then
-            iets = barrel
+            local inventory = barrel.list()
+            for barrel in pairs(inventory) do
+                barrel.name = barrel.name:gsub("minecraft:", "")
+                local text2 = barrel.name
+                local text = "chest"
+                local width = math.floor((x - #text) / 2)
+                local height = 1
+                monitor.setCursorPos(width, height)
+                monitor.write(text)
+            end
         end
     end
 end
 
-local x, y = monitor.getSize()
-monitor.setCursorPos(1, 1)
-monitor.write(x)
-monitor.setCursorPos(1, 2)
-monitor.write(y)
+while true do
+    local monitor = peripheral.find("monitor")
+    local chest = peripheral.find("minecraft:chest")
+    local barrel = peripheral.find("minecraft:barrel")
+    print_inventory_statistics(chest, barrel, monitor)
 
-if x < 36 then
-    monitor.setTextScale(4)
-    monitor.write("Monitor width is too small")
-else if y < 13 then
-    monitor.setTextScale(6)
-    monitor.write("Monitor high is too small")
-else
-    monitor.clear()
-    local inventory = iets.list()
-    if iets.list() == nil then
-        monitor.write("No items found")
-    end
-
-    for name in pairs(inventory) do
-        name.name = name.name:gsub("minecraft:", "")
-        local text = name.name
-        local width = math.floor((x - #text) / 2)
-        local height = 1
-        monitor.setCursorPos(width, height)
-        monitor.write(text)
-    end
-end
+    sleep(1)
 end
